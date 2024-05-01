fe21b19f39315720c132f82c6b604d7967792b24
Yash Shah <89223735+YashShah138@users.noreply.github.com>
2024-04-30 21:43:07 -0500

    ItemToPurchase.h

---
+++ ItemToPurchase.h
@@ -0,0 +1,27 @@
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
+#include <string>
+using namespace std;
+
+class ItemToPurchase {
+    public:
+        ItemToPurchase(string n = "none", string d = "none", double p = 0, int q = 0);
+        void SetName(string n);
+        void SetDescription(string d);
+        void SetPrice(double p);
+        void SetQuantity(int q);
+        string GetName() const;
+        string GetDescription() const;
+        double GetPrice() const;
+        int GetQuantity() const;
+        void PrintItemCost() const;
+        void PrintItemDescription() const;
+
+    private:
+        string name;
+        string itemDescription;
+        double price;
+        int quantity;
+};
+
+#endif
